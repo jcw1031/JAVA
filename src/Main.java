436bb1f3bada3f71e606fdb61c78d7061c031feb
woopaca <jcw001031@gmail.com>
2022-07-02 09:07:27 +0900

    slack test

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int total = 0;
-        int max = 0;
-        int tmp;
-        for (int i = 0; i < 10; i++) {
-            tmp = s.nextInt();
-            total -= tmp;
-            tmp = s.nextInt();
-            total += tmp;
-            if (max < total) max = total;
-        }
-        System.out.println(max);
-    } 
+        
+    }
 }
