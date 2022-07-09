19c5c8b07fe6d38a94f55283b9e8f584abb31ca0
woopaca <jcw001031@gmail.com>
2022-07-09 15:43:18 +0900

    coding test study dfs, bfs

--- src/CodingTestSilver/NumberOfIslands4963.java
+++ src/CodingTestSilver/NumberOfIslands4963.java
@@ -1,9 +1,11 @@
+package CodingTestSilver;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class NumberOfIslands4963 {
     public static int h, w;
     public static int[][] map;
 
