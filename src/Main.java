21d7ae0779d0b25a1a9b1a437849c5471a83b3f8
Woopaca <jcw001031@gmail.com>
2022-07-14 11:00:08 +0900

    windows test

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    public static ArrayList<HashMap<Integer, Integer>> list = new ArrayList<>();
     public static Queue<Integer> queue = new LinkedList<>();
     public static boolean[] visited;
     public static int[] degree;
     public static int[] order;
 
-    public static void bfs(int start) throws IOException {
+    public static void bfs(int start) {
         queue.offer(start);
         visited[start] = true;
         degree[start] = 0;
@@ -34,32 +33,24 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        visited = new boolean[n + 1];
-        degree = new int[n + 1];
-        order = new int[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            list.add(new ArrayList<>());
-            degree[i] = -1;
-        }
         int m = Integer.parseInt(st.nextToken());
-        int start = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < m; i++) {
+
+        for(int i=0;i<n+1;i++){
+            list.add(new HashMap<>());
+        }
+        for(int i=0;i<n-1;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            list.get(a).add(b);
-            list.get(b).add(a);
+            list.get(a).put(b, c);
+            list.get(b).put(a, c);
         }
-        for (int i = 0; i < list.size(); i++) {
-            Collections.sort(list.get(i));
-        }
-        bfs(start);
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
 
-        long sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += (long)order[i] * (long)degree[i];
+
         }
-        System.out.println(sum);
     }
 }
