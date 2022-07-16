315ec3b565f33ad5429a9314258fb6e11221f180
woopaca <jcw001031@gmail.com>
2022-07-16 19:57:01 +0900

    mola

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int t = Integer.parseInt(br.readLine());
-        for (; t > 0; t--) {
-            boolean flag = true;
-            String op = br.readLine();
-            int size = Integer.parseInt(br.readLine());
-            //String sequence = br.readLine();
-            st = new StringTokenizer(br.readLine(), "[],");
+        int n = Integer.parseInt(br.readLine());
+        int[][] time = new int[n][2];
+        int count = 0;
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
 
-            //sequence = sequence.substring(1, sequence.length() - 1);
-            //String[] arr = sequence.split(",");
-
-            ArrayList<Integer> list = new ArrayList<Integer>();
-
-            for (int i = 0; i < size; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
+        Arrays.sort(time, (o1, o2) -> {
+            if(o1[1] == o2[1]) return o1[0]-o2[0];
+            else{
+                return o1[1]-o2[1];
             }
-
-            try {
-                int pointer = 0;
-                for (int i = 0; i < op.length(); i++) {
-                    if (!flag) break;
-                    switch (op.charAt(i)) {
-                        case 'R': {
-                            if (pointer == 0) {
-                                pointer = list.size() - 1;
-                            } else {
-                                pointer = 0;
-                            }
-                            break;
-                        }
-                        case 'D': {
-                            if(list.size()==0) throw new Exception();
-                            list.remove(pointer);
-                            if (pointer != 0) {
-                                pointer--;
-                            }
-                            break;
-                        }
-                    }
-                }
-                if (pointer != 0) {
-                    Collections.reverse(list);
-                }
-                bw.write("[");
-                for(int i=0;i<list.size()-1;i++){
-                    bw.write(list.get(i)+",");
-                }
-                if(list.size()!=0) bw.write(""+list.get(list.size()-1));
-                bw.write("]\n");
-                bw.flush();
-            } catch (Exception e) {
-                System.out.println("error");
+        });
+        int end = time[0][1];
+        for(int i=1;i<n;i++){
+            if(time[i][0]>=end) {
+                end = time[i][1];
+                count++;
             }
         }
+        System.out.println(count+1);
     }
 }
