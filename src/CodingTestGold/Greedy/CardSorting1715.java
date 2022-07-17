9dad651f9fa21aa81667715d6ac90c47f7ec8d1b
woopaca <jcw001031@gmail.com>
2022-07-17 16:32:20 +0900

    coding test

--- src/CodingTestGold/Greedy/CardSorting1715.java
+++ src/CodingTestGold/Greedy/CardSorting1715.java
@@ -1,7 +1,9 @@
+package CodingTestGold.Greedy;
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class CardSorting1715 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
