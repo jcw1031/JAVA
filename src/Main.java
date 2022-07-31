14dca8360ff1fbad67fc08ce8024b40ba63357b6
woopaca <jcw001031@gmail.com>
2022-07-31 14:28:59 +0900

    coding test

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int n, k;
+    public static int[] count = new int[100001];
+    public static boolean[] visited = new boolean[100001];
+    public static Queue<Integer> queue = new LinkedList<>();
+
+    public static int bfs() {
+        while (!queue.isEmpty()) {
+            int tmp = queue.poll();
+
+            if (tmp == k) return count[k];
+
+            int[] a = new int[3];
+            a[0] = tmp - 1;
+            a[1] = tmp + 1;
+            a[2] = tmp * 2;
+
+            for (int i = 0; i < 3; i++) {
+                if (a[i]>=0 && a[i]<=100000 && !visited[a[i]] ) {
+                    count[a[i]] = count[tmp] + 1;
+                    queue.add(a[i]);
+                    visited[a[i]] = true;
+                }
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int[][] rgb = new int[n][3];
-        int[][] result = new int[n][3];
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            rgb[i][0] = Integer.parseInt(st.nextToken());
-            rgb[i][1] = Integer.parseInt(st.nextToken());
-            rgb[i][2] = Integer.parseInt(st.nextToken());
-        }
+        queue.add(n);
+        visited[n] = true;
+        int result = bfs();
 
-        result[0][0] = rgb[0][0];
-        result[0][1] = rgb[0][1];
-        result[0][2] = rgb[0][2];
-
-        for(int i=1;i<n;i++){
-            result[i][0] = (result[i-1][1]>result[i-1][2]?result[i-1][2]:result[i-1][1])+rgb[i][0];
-            result[i][1] = (result[i-1][0]>result[i-1][2]?result[i-1][2]:result[i-1][0])+rgb[i][1];
-            result[i][2] = (result[i-1][0]>result[i-1][1]?result[i-1][1]:result[i-1][0])+rgb[i][2];
-        }
-
-        int min = result[n-1][0];
-        for(int i=1;i<3;i++){
-            if(min>result[n-1][i]) min = result[n-1][i];
-        }
-        System.out.println(min);
+        System.out.println(result);
     }
 }
