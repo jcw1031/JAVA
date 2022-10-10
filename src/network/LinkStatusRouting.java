d3fc64be108484d3ca23b9c560451a71c1c2f9ae
woopaca <jcw001031@gmail.com>
2022-10-10 17:55:59 +0900

    우선순위 큐 적용 시도

--- src/network/LinkStatusRouting.java
+++ src/network/LinkStatusRouting.java
@@ -1,28 +1,19 @@
 package network;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.annotation.Documented;
+import java.util.*;
 
 /**
  * 노드 클래스
  */
 class Node{
     private String name; //문자 이름
-//    private ArrayList<Node> link = new ArrayList<>();
     private boolean visited; //방문 여부
-    /*private int[] next;
-    private int[] cost;*/
     private Map<Node, Integer> lsp = new HashMap<>(); //링크 상태 패킷
-    private ArrayList<Map<Node, IntegerAndNode>> routingTable = new ArrayList<>(); //라우팅 테이블
+    private Map<Node, IntegerAndNode> routingTable = new HashMap<>(); //라우팅 테이블
 
     public Node(String name){
         this.name = name;
-        /*next = new int[n+1];
-        cost = new int[n+1];*/
-        for(int i = 0; i<= LinkStatusRouting.N; i++){
-            routingTable.add(new HashMap<>());
-        }
     }
 
     public String getName() {
@@ -46,13 +37,27 @@ class Node{
     }
 
     public void setLsp(Node node, int cost) {
-        lsp.put(node, cost);
+        this.lsp.put(node, cost);
+    }
+
+    public Map<Node, IntegerAndNode> getRoutingTable() {
+        return routingTable;
     }
 
     /**
-     * 라우팅 테이블에 비용과 다음 노드를 함께 저장하기 위한 클래스
+     * 라우팅 테이블 수정
+     * @param node
+     * @param cost
+     * @param nextNode
      */
-    class IntegerAndNode{
+    public void setRoutingTable(Node node, int cost, Node nextNode){
+        this.routingTable.put(node, new IntegerAndNode(cost, nextNode));
+    }
+
+    /**
+     * 라우팅 테이블에 비용과 다음 노드를 함께 저장하기 위한 Node 클래스의 내부 클래스
+     */
+    static class IntegerAndNode{
         int cost;
         Node nextNode;
 
@@ -84,35 +89,89 @@ public class LinkStatusRouting {
         setLsp(); //링크 상태 패킷 생성
         setDistance();
         printLsdb();
+        printRoutingTable();
+        flooding();
+        printRoutingTable();
     }
 
     /**
      * 플러딩 알고리즘
      */
-    public void flooding() {
+    private static void flooding() {
+        PriorityQueue<Node.IntegerAndNode> pq = new PriorityQueue<>(new Comparator<Node.IntegerAndNode>() {
+            @Override
+            public int compare(Node.IntegerAndNode i1, Node.IntegerAndNode i2){
+                if(i1.getCost() < i2.getCost()){
+                    return -1;
+                }
+                else if(i1.getCost() > i2.getCost()){
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+            }
+        });
 
+        Queue<Node> queue = new LinkedList<>();
+        for(int i=1;i<=N;i++){
+            clearVisited();
+            nodes[i].setVisited(true);
+            for(Node key : nodes[i].getLsp().keySet()){
+                pq.add(new Node.IntegerAndNode(nodes[i].getLsp().get(key), key));
+                queue.add(key);
+            }
+
+            while(!pq.isEmpty()){
+                Node tmp = pq.poll().getNextNode();
+            }
+
+
+
+
+            while(!queue.isEmpty()){
+                Node tmp = queue.poll();
+                for(Node key : tmp.getLsp().keySet()){
+                    if(!key.isVisited()){
+                        key.setVisited(true);
+                        queue.add(key);
+                    }
+                    int newCost = nodes[i].getRoutingTable().get(tmp).getCost() + tmp.getLsp().get(key);
+                    if(nodes[i].getRoutingTable().get(key).getCost() > newCost){
+                        nodes[i].setRoutingTable(key, newCost, tmp);
+                    }
+                }
+            }
+        }
     }
 
     /**
      * 노드 생성
      */
-    public static void makeNode() {
+    private static void makeNode() {
         nodes = new Node[N + 1];
 
         for (int i = 0; i <= N; i++) {
             nodes[i] = new Node(Character.toString((char) (i + 64)));
+        }
+
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                nodes[i].setRoutingTable(nodes[j], INF, null);
+            }
         }
     }
 
     /**
      * 링크 상태 패킷 생성
      */
-    public static void setLsp() {
+    private static void setLsp() {
         for (int i = 1; i <= N; i++) {
             nodes[i].setLsp(nodes[i], 0);
         }
         nodes[findByName("A")].setLsp(nodes[findByName("B")], 5);
         nodes[findByName("A")].setLsp(nodes[findByName("C")], 6);
+        nodes[findByName("A")].setLsp(nodes[findByName("D")], 4);
         nodes[findByName("B")].setLsp(nodes[findByName("A")], 5);
         nodes[findByName("B")].setLsp(nodes[findByName("D")], 5);
         nodes[findByName("B")].setLsp(nodes[findByName("F")], 4);
@@ -128,9 +187,16 @@ public class LinkStatusRouting {
         nodes[findByName("E")].setLsp(nodes[findByName("G")], 4);
         nodes[findByName("F")].setLsp(nodes[findByName("B")], 4);
         nodes[findByName("F")].setLsp(nodes[findByName("G")], 2);
+        nodes[findByName("G")].setLsp(nodes[findByName("C")], 7);
         nodes[findByName("G")].setLsp(nodes[findByName("D")], 5);
         nodes[findByName("G")].setLsp(nodes[findByName("E")], 4);
         nodes[findByName("G")].setLsp(nodes[findByName("F")], 2);
+
+        for(int i=1;i<=N;i++){
+            for(Node key : nodes[i].getLsp().keySet()){
+                nodes[i].setRoutingTable(key, nodes[i].getLsp().get(key), null);
+            }
+        }
     }
 
     /**
@@ -138,7 +204,7 @@ public class LinkStatusRouting {
      * @param name
      * @return int
      */
-    public static int findByName(String name) {
+    private static int findByName(String name) {
         for(int i=1;i<=N;i++){
             if(nodes[i].getName().equals(name)){
                 return i;
@@ -150,7 +216,7 @@ public class LinkStatusRouting {
     /**
      * 초기 LSDB 구축
      */
-    public static void setDistance(){
+    private static void setDistance(){
         for(int i=1;i<=N;i++){
             for(int j=1;j<=N;j++){
                 distance[i][j] = INF;
@@ -164,7 +230,7 @@ public class LinkStatusRouting {
         }
     }
 
-    public static void printLsdb(){
+    private static void printLsdb(){
         for(int i=0;i<=N;i++){
             for(int j=0;j<=N;j++){
                 if(i==0 && j==0){
@@ -181,11 +247,39 @@ public class LinkStatusRouting {
                         System.out.print(distance[i][j] + "\t");
                     }
                     else{
-                        System.out.print(-1+"\t"); //무한대인 경우 -1로 표기
+                        System.out.print("∞\t"); //무한대인 경우 -1로 표기
                     }
                 }
             }
             System.out.println();
+        }
+    }
+
+    private static void printRoutingTable(){
+        for(int i=1;i<=N;i++){
+            System.out.println("----------"+nodes[i].getName()+" 노드 라우팅 테이블----------");
+
+            for(int j=1;j<=N;j++){
+                System.out.print(nodes[j].getName()+"\t");
+                if(nodes[i].getRoutingTable().get(nodes[j]).getCost() == INF){
+                    System.out.print("∞\t");
+                }
+                else{
+                    System.out.print(nodes[i].getRoutingTable().get(nodes[j]).getCost()+"\t");
+                }
+                if(!(nodes[i].getRoutingTable().get(nodes[j]).getNextNode() == null)){
+                    System.out.println(nodes[i].getRoutingTable().get(nodes[j]).getNextNode().getName());
+                }
+                else{
+                    System.out.println("-");
+                }
+            }
+        }
+    }
+
+    private static void clearVisited(){
+        for(int i=1;i<=N;i++){
+            nodes[i].setVisited(false);
         }
     }
 }
